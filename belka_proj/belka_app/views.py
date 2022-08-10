1715dba47dfef9fbd220ad21325577405ecef010
blind-stranger <sofyaryabchenko21@gmail.com>
2022-08-10 14:11:41 +0700

    add "about"

--- belka_proj/belka_app/views.py
+++ belka_proj/belka_app/views.py
@@ -5,3 +5,6 @@ def index(request):
     photos = Photo.objects.all()
     photos_dict = {'photos': photos}
     return render(request, 'index.html', context=photos_dict)
+
+def about(request):
+    return render(request, 'about me.html', context={})
--- belka_proj/belka_proj/urls.py
+++ belka_proj/belka_proj/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from belka_app.views import index
+from belka_app.views import about
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+    path('', index, name="index"),
+    path('about_me/', about, name="about")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
