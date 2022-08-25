2f0af25893d40a990c77662f8ca4a18720491cdb
blind-stranger <sofyaryabchenko21@gmail.com>
2022-08-25 17:31:47 +0700

    for loop

--- belka_proj/belka_app/models.py
+++ belka_proj/belka_app/models.py
@@ -2,3 +2,4 @@ from django.db import models
 
 class Photo(models.Model):
     image = models.ImageField()
+    priority = models.PositiveSmallIntegerField(default=0)
--- belka_proj/belka_app/views.py
+++ belka_proj/belka_app/views.py
@@ -3,8 +3,29 @@ from belka_app.models import Photo
 
 def index(request):
     photos = Photo.objects.all()
-    photos_dict = {'photos': photos}
-    return render(request, 'index.html', context=photos_dict)
+    count = 0
+    column_1 = []
+    column_2 = []
+    column_3 = []
+
+    for i in photos:
+        if count > 2:
+            count = 0
+        if count == 0:
+            column_1.append(i)
+        elif count == 1:
+            column_2.append(i)
+        elif count == 2:
+            column_3.append(i)
+        count += 1
+
+    context_photo_dict = {
+        "column_1" : column_1,
+        "column_2" : column_2,
+        "column_3" : column_3
+    }
+
+    return render(request, 'index.html', context=context_photo_dict)
 
 def about(request):
     return render(request, 'about me.html', context={})
